aad75863d764b2c7e99c367a38a33804c30f1607
ssj <1263914174@qq.com>
2020-12-08 18:30:25 +0800

    hello ssj

--- soot-infoflow-cmd/src/soot/jimple/infoflow/cmd/MainClass.java
+++ soot-infoflow-cmd/src/soot/jimple/infoflow/cmd/MainClass.java
@@ -242,6 +242,7 @@ public class MainClass {
 
 	public static void main(String[] args) throws Exception {
 		MainClass main = new MainClass();
+		System.err.println("Hello ssj!");
 		main.run(args);
 	}
 
